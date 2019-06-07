f3e6591d4e27c183e8970e4e0ea4cf603c0a265b
Manas <prometheansacrifice@gmail.com>
2019-06-07 15:27:16 +0530

    Windows CI: Use esy build instead of pkg-config

--- bootstrap.sh
+++ bootstrap.sh
@@ -92,10 +92,11 @@ jobs:
   - script: esy install
     displayName: 'esy install'
   - \${{ if eq(parameters.host, 'Windows') }}:
-    - script: esy b bash .ci/pkg-config-cygwin.sh
-      displayName: 'Installing pkg-config in cygwin'
-  - script: 'esy x pkg-config --libs <COMMAND>'
-    displayName: 'Checking library...'
+    - script: esy b
+      displayName: 'esy build'
+  - \${{ if ne(parameters.host, 'Windows') }}:
+    - script: 'esy x pkg-config --libs <COMMAND>'
+      displayName: 'esy x pkg-config --libs <COMMAND>'
 EOF
 
     cat >.gitattributes<<EOF
@@ -116,14 +117,14 @@ EOF
     "exportedEnv": {
       "PKG_CONFIG_PATH": {
         "scope": "global",
-        "val": "#{self.lib / 'pkgconfig' : $PKG_CONFIG_PATH }"
+        "val": "#{self.lib / 'pkgconfig' : \$PKG_CONFIG_PATH }"
       }
     },
     "build": [
-      ["chmod", "755", "./configure"],
-      ["bash", "-c", "#{os == 'windows' ? './configure --prefix=\$cur__install --host x86_64-w64-mingw32' : './configure --prefix=\$cur__install'}"],
-      ["make"],
-      ["make", "install"]
+      "chmod 755 ./configure",
+      "./configure --prefix=#{self.install} #{os == 'windows' ? '--host=x86_64-w64-mingw32' : ''}",
+      "make",
+      "make install"
     ]
   },
   "dependencies": {},
